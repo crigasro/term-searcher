53dd2cfa26131e25fcdc726c72d21bed49915d85
crigasro <gasparrozalen@gmail.com>
2021-08-24 16:53:25 +0200

    Added logic to search terms at home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { TermHelperService } from './../../services/term-helper.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  searchNotifier = new Subject<string>();
+
+  constructor(
+    private termHelper: TermHelperService
+  ) { }
 
   ngOnInit(): void {
+    this.searchNotifier
+      .pipe(debounceTime(500))
+      .subscribe(input => this.searchItems(input));
   }
 
-  handleSearch(input: string) {
-    console.log(input);
+  ngOnDestroy() {
+		this.searchNotifier.complete();
+	}
+
+  searchItems(input: string) {
+    if (!input) {
+      return;
+    }
+    this.termHelper.getSearchedResults(input)
+      .pipe(takeUntil(this.searchNotifier))
+      .subscribe(
+        res => {
+          console.log(res.totalItems);
+        },
+        err => {
+          console.log(err);
+        }
+      );
   }
 
 }
